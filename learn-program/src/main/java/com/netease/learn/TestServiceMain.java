c63e331f97b3bc14682aab5f07bbe1a00ee14cd2
LingJun123 <56064594+LingJun123@users.noreply.github.com>
2024-02-05 17:46:56 +0800

    初始化

---
+++ learn-program/src/main/java/com/netease/learn/TestServiceMain.java
@@ -0,0 +1,11 @@
+package com.netease.learn;
+
+/**
+ * @Author wangbin16
+ * @Date 2024/2/5 17:06
+ */
+public class TestServiceMain {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
